f9c5d30d26e799de7feb8fce15fc7475ad29eaa7
Mariano Mercado <maomercado12@gmail.com>
2024-03-17 18:03:38 -0300

    sist de ventas

---
+++ SistemaVentas/SistemaVentas/Conexion.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Conexion
+
+    Protected ccn As New SqlConnection
+    Protected cmd As New SqlCommand
+
+    Public Sub conectado()
+        ccn = New SqlConnection("Data Source=.;Initial Catalog=SistemaVentas;Integrated Security=True")
+        ccn.Open()
+    End Sub
+
+End Class
